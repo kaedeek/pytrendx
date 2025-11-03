7f759628d1e37c8e20be1e76a6a3e682fc5e48c5
kaedeek <yukise30@icloud.com>
2025-11-04 01:04:49 +0900

    __init__.py add

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="pytrendx",
-    version="1.0.2",
+    version="1.0.3",
     description="Fetch and visualize PyPI package download trends directly from the terminal.",
     long_description=long_description,
     long_description_content_type="text/markdown",
